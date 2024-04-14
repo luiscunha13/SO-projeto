b9db8741d979db61f8588f997cbdb4b1681f84e5
Luís Cunha <luispintocunha@hotmail.com>
2024-04-14 16:45:57 +0100

    cliente quase feito (em princípio)

--- src/client.c
+++ src/client.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
+#include <stdlib.h>
 #include<string.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #define MAX 300
 
 int main(int argc, char * argv[]){
+
+    char comand[MAX];
 
     if (argc < 2){
         printf("não tem argumentos suficientes\n");
@@ -12,11 +17,26 @@ int main(int argc, char * argv[]){
     }
 
     if(strcmp(argv[1],"execute") == 0){
-
+        sprintf(comand,"%s %d %s %s", argv[1], atoi(argv[2]), argv[3], argv[4]);
     }
     else if(strcmp(argv[1],"status") == 0){
-
+        strcpy(comand,"status");
     }
-    else printf("comando inválido");
+    //else printf("comando inválido");
+
+    int cs_fifo = open("client_server", O_WRONLY);
+    if(cs_fifo<0) perror("erro abrir fifo client_server");
+
+    write(cs_fifo,comand,strlen(comand));
+    close(cs_fifo);
+
+    size_t b_read;
+    int server_client = open("server_client",O_RDONLY);
+    while((b_read = read(server_client,comand,MAX))>0){
+        write(...,comand,b_read); //perceber onde se deve escrever a resposta
+    }
+    close(server_client);
+
+    return 0;
 
 }
--- src/orchestrator.c
+++ src/orchestrator.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #define MAX 300
 
@@ -18,10 +22,17 @@ int main(int argc, char *argv[]){
         perror("mkfifo server-client");
         return -1;
     }
-    //abre o fifo para leitura
-    int server_fifo_fd = open(CLIENT_SERVER, O_RDONLY);
-    if (server_fifo_fd == -1) {
-        perror("server_fifo open");
+    //abre o fifo client-server para leitura
+    int client_server = open(CLIENT_SERVER, O_RDONLY);
+    if (client_server == -1) {
+        perror("client-server open");
+        return -1;
+    }
+
+    //abre fifo server-client para escrita
+    int server_client = open(SERVER_CLIENT, O_WRONLY);
+    if (server_client == -1) {
+        perror("server-client open");
         return -1;
     }
 
@@ -29,7 +40,15 @@ int main(int argc, char *argv[]){
 
     while(1){
         char *instruction = malloc(MAX);
-        if(server_fifo_fd)
+        read(client_server,instruction, MAX);
+
+        if(strncmp(instruction, "execute", 7) == 0){
+
+        }
+        else if(strncmp(instruction, "status", 6)==0){
+            
+        }
+
     }
 
 
